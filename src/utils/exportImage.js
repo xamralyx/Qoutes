c46416f08145bbe6e854c86b194a12b01a67042d
xamralyx <amrdiaamostafa@gmail.com>
2026-01-19 18:12:52 -0500

    chore(cleanup): remove unused exportImage utility placeholder

--- src/utils/exportImage.js
+++
@@ -1,5 +0,0 @@
-export async function exportNodeAsImage(node) {
-  if (!node) throw new Error('No node provided');
-  // Placeholder: integrate html2canvas or similar in feature branch
-  return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUA';
-}
